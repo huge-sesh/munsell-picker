5004f6b68d1787326fe96da1c2cb8fcf95ef04b2
sesh <jwgforward@gmail.com>
2014-03-20 13:41:05 -0400

    Initial Commit

---
+++ munsell wheel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  munsell wheel
+//
+//  Created by jeff on 3/20/14.
+//  Copyright (c) 2014 jeff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ munsell wheel/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  munsell wheel
+//
+//  Created by jeff on 3/20/14.
+//  Copyright (c) 2014 jeff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
